2282c3c029188c304e1044f065d91afdaaaeb93b
João Vitor Bruno Cunha Santos <Joaovitorbrunocunhasantos@gmail.com>
2022-11-15 15:33:07 -0300

    para caio

---
+++ proje.js
@@ -0,0 +1,12 @@
+let ul = document.querySelector('nav ul')
+function max(){
+
+   
+   if(ul.classList.contains('max-hin')){
+      ul.classList.remove('max-hin');
+      
+   }else{
+      ul.classList.add('max-hin');
+
+   }
+}
